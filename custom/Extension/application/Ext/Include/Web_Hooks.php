6906fc8270605cd4e6d54a75075a4bf9eb6fb8bc
SidorkinAlex <palach.orl@mail.ru>
2021-04-11 09:32:55 +0300

    added Module WEB_HOOKS

---
+++ custom/Extension/application/Ext/Include/Web_Hooks.php
@@ -0,0 +1,9 @@
+<?php 
+ //WARNING: The contents of this file are auto-generated
+$beanList['WEBH_HOOKS_REQUEST'] = 'WEBH_HOOKS_REQUEST';
+$beanFiles['WEBH_HOOKS_REQUEST'] = 'modules/WEBH_HOOKS_REQUEST/WEBH_HOOKS_REQUEST.php';
+$modules_exempt_from_availability_check['WEBH_HOOKS_REQUEST'] = 'WEBH_HOOKS_REQUEST';
+$report_include_modules['WEBH_HOOKS_REQUEST'] = 'WEBH_HOOKS_REQUEST';
+$modInvisList[] = 'WEBH_HOOKS_REQUEST';
+
+?>
