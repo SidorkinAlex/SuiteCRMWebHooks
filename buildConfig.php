bf85f2f7341597f0264c41f2665e91dad2b43c35
SidorkinAlex <palach.orl@mail.ru>
2021-04-13 23:21:26 +0300

    add builder backege and manifest

---
+++ buildConfig.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: alex
+ * Date: 12.04.21
+ * Time: 22:15
+ */
+$path_arr_for_build = array(
+    array(
+        'from' => 'modules/AOW_WorkFlow/aow_utils.php',
+        'to' => 'SugarModules/modules/AOW_WorkFlow/aow_utils.php',
+    ),
+    array(
+        'from' => 'modules/WEBH_HOOKS_REQUEST',
+        'to' => 'SugarModules/modules/WEBH_HOOKS_REQUEST',
+    ),
+    array(
+        'from' => 'icons/',
+        'to' => 'icons/',
+    ),
+    array(
+        'from' => 'LICENSE.txt',
+        'to' => 'LICENSE.txt',
+    ),
+    array(
+        'from' => 'manifest.php',
+        'to' => 'manifest.php',
+    ),
+    array(
+        'from' => 'custom/Extension/application/Ext/Language/en_us.Web_Hooks.php',
+        'to' => 'SugarModules/language/application/en_us.lang.php',
+    ),
+    array(
+        'from' => 'custom/Extension/application/Ext/Language/ru_ru.Web_Hooks.php',
+        'to' => 'SugarModules/language/application/ru_ru.lang.php',
+    ),
+);
+
+return $path_arr_for_build;
