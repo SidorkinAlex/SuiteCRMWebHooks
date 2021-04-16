ca75666ff3a1c2b25c6d89fd416347ebb2a283bb
SidorkinAlex <palach.orl@mail.ru>
2021-04-16 10:02:48 +0300

    added build Packedg

--- custom/Extension/modules/Contacts/Ext/Vardefs/_override_sugarfield_jjwg_maps_lng_c.php
+++
@@ -1,5 +0,0 @@
-<?php
- // created: 2021-03-14 15:21:54
-$dictionary['Contact']['fields']['jjwg_maps_lng_c']['inline_edit']=1;
-
- ?>
--- custom/Extension/modules/Leads/Ext/Vardefs/_override_sugarfield_jjwg_maps_lat_c.php
+++
@@ -1,5 +0,0 @@
-<?php
- // created: 2021-03-14 15:21:54
-$dictionary['Lead']['fields']['jjwg_maps_lat_c']['inline_edit']=1;
-
- ?>
